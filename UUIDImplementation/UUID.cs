fa6458b8f093414f533c2c9f3a625d7b5e23d28a
navin <navin@DESKTOP-2RT4O37>
2022-03-14 23:04:29 +1100

    Code to find longest increasing subsequence implemented.

--- UUIDImplementation/UUID.cs
+++ UUIDImplementation/UUID.cs
@@ -7,7 +7,55 @@ namespace UUIDImplementation
     {
         public string FindLongestIncreasingSubsequence(string sequence)
         {
-            throw new NotImplementedException();
+            var inputArray = (sequence ?? "").Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+            var lengthInputArray = inputArray.Length;
+            
+            int? lengthOfMaxSubsequence = null, startIndexOfMaxSubsequence = null, tempLength = null, tempStart = null;
+            int? previousElement = null;
+
+            for (var i = 0; i < lengthInputArray; ++i)
+            {
+                int currentElement;
+
+                if (!int.TryParse(inputArray[i], out currentElement))
+                {
+                    throw new ArgumentException(string.Concat(inputArray[i], " is invalid integer!"), "input");
+                }
+
+                if (previousElement != null)
+                {
+                    if (currentElement > previousElement)
+                    {
+                        if (tempStart == null)
+                        {
+                            tempStart = i - 1;
+                        }
+                        tempLength = tempLength == null ? 2 : tempLength + 1;
+                    }
+
+                    if ((currentElement <= previousElement || i == lengthInputArray - 1) && tempLength != null)
+                    {
+                        if (lengthOfMaxSubsequence == null || tempLength > lengthOfMaxSubsequence)
+                        {
+                            lengthOfMaxSubsequence = tempLength;
+                            startIndexOfMaxSubsequence = tempStart;
+                        }
+
+                        tempStart = tempLength = null;
+                    }
+                }
+
+                previousElement = currentElement;
+            }
+
+            if (lengthOfMaxSubsequence == null)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return string.Join(separator: ' ', value: inputArray, startIndex: startIndexOfMaxSubsequence.Value, count: lengthOfMaxSubsequence.Value);
+            }
         }
     }
 }
